6096350169d7c1dccf5b1b9aa68cc61d341034cd
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-02-26 18:39:23 +0200

    [feature] - Endpoint /events to store an event

---
+++ spec/requests/events/create_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe 'Create Event' do
+  let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+  let(:code) { SecureRandom.hex }
+  let(:params) do
+    {
+      event_class: event_class,
+      event_type: event_type,
+      stream_name: stream_name,
+      payload: {
+        code: code
+      }
+    }.compact
+  end
+
+  context 'when Command event' do
+    let(:event_class) { 'Command' }
+    let(:event_type) { 'CreateOrder' }
+    let(:payload) { { code: code } }
+    let(:stream_name) { nil }
+
+    it 'creates the event' do
+      post '/events', params: params.to_json, headers: headers
+
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response).to have_http_status(:created)
+
+      event_created = EventStore.event_store.read.last
+      expect(event_created).to(be_an_event(::Events::Commands::CreateOrder))
+      expect(event_created.data).to eq('code' => code)
+    end
+
+    context 'when stream name is specified' do
+      let(:stream_name) { 'foo stream' }
+
+      it 'publishes the event to the stream name' do
+        post '/events', params: params.to_json, headers: headers
+
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(response).to have_http_status(:created)
+
+        event_created = EventStore.event_store.read.stream(stream_name).last
+        expect(event_created).to(be_an_event(::Events::Commands::CreateOrder))
+        expect(event_created.data).to eq('code' => code)
+      end
+    end
+  end
+end
